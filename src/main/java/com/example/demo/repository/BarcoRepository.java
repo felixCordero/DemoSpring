030dcb7244520bac3e1c09bbfd7d3b6e07deb8d9
Felix <daldinen@gmail.com>
2024-11-14 21:13:49 +0100

    Añadido barcoRepository con consultas personalizadas

--- src/main/java/com/example/demo/repository/BarcoRepository.java
+++ src/main/java/com/example/demo/repository/BarcoRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Barco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +11,12 @@ public interface BarcoRepository extends JpaRepository<Barco, Long> {
     List<Barco> findByNombre(String nombre);
     List<Barco> findByEsloraGreaterThan(Integer eslora);
     List<Barco> findByNombreOrderByCapacidadDesc(String nombre);
+    @Query("SELECT b FROM Barco b WHERE b.nombre = :nombre")
+    List<Barco> findByNombre2(@Param("nombre") String nombre);
+    @Query("SELECT b FROM Barco b WHERE b.eslora > :eslora")
+    List<Barco> findByEsloraGreaterThan2(@Param("eslora") Integer
+                                                eslora);
+    @Query(value = "SELECT * FROM Barco b WHERE b.nombre = :nombre ORDER BY b.capacidad DESC", nativeQuery = true) //SQL
+            List<Barco> findByNombreOrderByCapacidadDesc2(@Param("nombre")
+            String nombre);
 }
